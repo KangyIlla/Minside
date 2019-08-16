072990bab41b6626b44783a50b586cc2d74bb828
Kangeyan Illavalagan <kangeyanillavalagan@1x-193-157-160-88.uio.no>
2019-08-16 17:00:19 +0200

    Updates

---
+++ precache-manifest.4a98aedbc3f94c8e48e68d76871d8ddd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97f4daf60217b46c58d3e0cd7f5a78ef",
+    "url": "/Minside/index.html"
+  },
+  {
+    "revision": "634a1912c5c6bd2356e3",
+    "url": "/Minside/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "06f7655ffc5fd762f4f9",
+    "url": "/Minside/static/js/2.fb007396.chunk.js"
+  },
+  {
+    "revision": "634a1912c5c6bd2356e3",
+    "url": "/Minside/static/js/main.2c636ad4.chunk.js"
+  },
+  {
+    "revision": "12382c50079813fa3c2a",
+    "url": "/Minside/static/js/runtime~main.9981f551.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Minside/static/media/logo.5d5d9eef.svg"
+  }
+]);
